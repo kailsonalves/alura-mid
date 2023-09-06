a3653946db21de00982aadbfefe99da89c331fa7
kailsonalves <106539606+kailsonalves@users.noreply.github.com>
2023-09-06 09:52:05 -0300

    Add files via upload

--- mains.js
+++ mains.js
@@ -2,4 +2,6 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom')
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+<button onclick="tocaSomPom()" class="tecla tecla_pom">Pom</button>
